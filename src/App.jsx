74085eaf21918931e906cc5242b2f4ea0e936c8b
yoniflutter <yohannessintayehu1@gmail.com>
2023-08-04 07:58:36 +0300

    files changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import './styles.css';
+import Header from "./components/Header";
+import "./styles.css";
 
 function App() {
   return (
     <div className="Home">
+      <div className="Container">
+        <Header />
+        <div className="Home-A">
+          <h1>Unlimted movies, Tv shows, and more</h1>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import  logo from '../assets/images/logo.png';
 const Header = () => {
     return ( 
         <div className="Header">
-            
+            <nav>
+                <div className="Logo">
+                    <img src={logo} height={"44px"} alt="Logo" />
+                </div>
+                <div className="Btn">
+                    <button className='Signin'>Sign In</button>
+                </div>
+            </nav>
         </div>
     );
 }
